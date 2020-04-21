3e20ec8dc4b763e75edaf6828535be03326c5f13
Hasan Ali <hasan1998ali@rediffmail.com>
2020-04-21 11:05:01 +0530

    Changed many functionalities

--- app.js
+++ app.js
@@ -1,63 +1,92 @@
 //if user adds a note then add it to local storage
-showNotes();
+//showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", function (e) {
     let addtxt = document.getElementById("addTxt");
-    let addTitleTxt=document.getElementById("addTxtTitle");
+    let addTitleTxt = document.getElementById("addTxtTitle");
     let notes = localStorage.getItem("notes");
+    let blankHtml = "";
     if (notes == null) {
         notesAr = [];
     }
     else {
         notesAr = JSON.parse(notes);
     }
-    let myObj=
+    let myObj =
     {
-        text:addtxt.value,
-        title:addTitleTxt.value
+        text: addtxt.value,
+        title: addTitleTxt.value
     };
-
     notesAr.push(myObj);
+    console.log(myObj)
     localStorage.setItem("notes", JSON.stringify(notesAr));
     addtxt.value = "";
-    addTitleTxt.value="";
-
-
-
-
-    showNotes();
+    addTitleTxt.value = "";
+    if (myObj.text != "" && myObj.title != "") {
+        alertBox('success', "Your notes has been successfully submitted");
+    }
+    else {
+        alertBox('danger', "Sorry note can't be blank");
+    }
+   // showNotes();
 
 });
+
+
+//function to show alert
+
+function alertBox(type, message) {
+    let alertElement = document.getElementById('alert');
+    blankHtml = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+     <strong>${message}</strong>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>`;
+    alertElement.innerHTML = blankHtml;
+    setTimeout(() => {
+        alertElement.innerHTML = '';
+    }, 2000);
+}
 //function to show elements
 function showNotes() {
     let html = "";
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesAr = [];
-        
+
     }
     else {
         notesAr = JSON.parse(notes);
     }
 
     notesAr.forEach(function (element, index) {
-        if(element.title!="")
-        {
-        html += `
-        <div class="noteCrd my-2 card mx-2" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">${element.title}</h5>
-            <p class="card-text">${element.text}</p>
-              <button id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
-              <button id=Imp_${index} onclick="markNote(this.id)" class="btn btn-primary">Mark Note</button>
-            </div>
-          </div>`;
+        if (element.title != "" && element.text != "") {
+            html +=
+                `
+            <div class="card">
+              <div class="card-header" id="heading${index}">
+                <h2 class="mb-0">
+                  <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
+                    ${element.title}
+                  </button>
+                </h2>
+              </div>
+          
+              <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#notes">
+                <div class="card-body">
+                  ${element.text}
+                </div>
+                <button id=${index} onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                 <button onclick="dictateNote('${element.text}');" class="btn btn-primary">Dictate Note</button>
+              </div>
+            </div>`;
         }
     });
 
 
     let notesElm = document.getElementById('notes');
-    let nottTitle=document.getElementById("divTitle");
+    let nottTitle = document.getElementById("divTitle");
     if (notesAr.length != 0) {
 
         notesElm.innerHTML = html;
@@ -68,8 +97,15 @@ function showNotes() {
 
 }
 
+function dictateNote(text) {
+     responsiveVoice.speak(text);
+    // responsiveVoice.speak(text, "UK English Male")
+    console.log(text);
+
+}
+
 function deleteNote(index) {
-    let yesOrNo = confirm("Do yoo wanna delete the note?");  //for confirmation from the user
+    let yesOrNo = confirm("Do you want to delete this note?");  //for confirmation from the user
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesAr = [];
@@ -100,5 +136,21 @@ srchTxt.addEventListener('input', function (element) {
     });
 })
 
-//function to mark important
-    
+//function to view the note
+
+let viewBtn=document.getElementById('viewBtn');
+viewBtn.addEventListener('click',()=>
+{
+    let notesElm = document.getElementById('notes');
+    if(notesElm.style.display!='inline' && viewBtn.innerText!="Hide Note")
+    {
+
+        showNotes();
+        viewBtn.innerText="Hide Note"
+        notesElm.style.display='inline';
+    }
+    else{
+        notesElm.style.display='none';
+        viewBtn.innerText="View Note"
+    }
+})
